77ec7384197e6a744b86dd5d6ac09900b4b66af0
Marcus Tillmanns <marcus.tillmanns@qt.io>
2024-06-23 21:53:17 +0200

    Send BLE commands directly
    Instead of using the vehicle-cmd, use the tesla packages directly to connect and communicate with the car.

--- main.go
+++ main.go
@@ -1,71 +1,155 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"os/exec"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/teslamotors/vehicle-command/pkg/connector/ble"
+	"github.com/teslamotors/vehicle-command/pkg/protocol"
+	"github.com/teslamotors/vehicle-command/pkg/protocol/protobuf/universalmessage"
+	"github.com/teslamotors/vehicle-command/pkg/vehicle"
+	"golang.org/x/sync/semaphore"
 )
 
-func runCmd(w io.Writer, args ...string) () {
-	cmd := exec.Command("./tesla-cmd", args...)
-	stdout, err := cmd.Output()
+type CarCallback func(*vehicle.Vehicle, context.Context) error
 
+func withCarConnection(cb CarCallback, needsInfotainment bool) (error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 8*time.Second)
+	defer cancel()
+
+	vin := os.Getenv("TESLA_VIN")
+	privateKeyFile := os.Getenv("TESLA_KEYFILE")
+
+	privateKey, err := protocol.LoadPrivateKey(privateKeyFile);
 	if err != nil {
-		e := err.(*exec.ExitError)
-
-		fmt.Fprintf(w, string(stdout))
-		fmt.Fprintf(w, string(e.Stderr))
-		fmt.Fprintf(w, err.Error())
-		return
+		fmt.Println("Error loading private key: ", privateKeyFile, err)
+		return err
 	}
 
-	// Print the output
-	fmt.Fprintf(w, string(stdout))
+	//fmt.Println("Connecting to car with VIN", vin)
+	conn, err := ble.NewConnection(ctx, vin)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	//fmt.Println("Creating vehicle object...")
+	car, err := vehicle.NewVehicle(conn, privateKey, nil)
+	if err != nil {
+		return nil
+	}
+
+	//fmt.Println("Connecting to car...")
+	if err = car.Connect(ctx); err != nil {
+		return err
+	}
+	defer car.Disconnect()
+
+	if err = car.StartSession(ctx, []universalmessage.Domain{universalmessage.Domain_DOMAIN_VEHICLE_SECURITY}); err != nil {
+		return errors.New("ErrNotAvailable")
+	}
+
+	if needsInfotainment {
+		if err = car.StartSession(ctx, []universalmessage.Domain{universalmessage.Domain_DOMAIN_INFOTAINMENT}); err != nil {
+			return errors.New("ErrAsleep")
+		}
+	}
+
+	//fmt.Println("Running callback...")
+	return cb(car, ctx)
+}
+
+func handleFunc(w http.ResponseWriter, r *http.Request, cb CarCallback, needsInfotainment bool) {
+	err := withCarConnection(cb, needsInfotainment)
+
+	// We need to give the BLE device a second to recover for the next connection
+	// otherwise subsequent connections will fail.
+	time.Sleep(1 * time.Second) 
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Println("Error: ", err)
+		fmt.Fprint(w, err)
+	} else {
+		w.WriteHeader(http.StatusOK)
+		fmt.Println("Success")
+		fmt.Fprint(w, "Success")
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/charging-set-amps/{amps}", func(w http.ResponseWriter, r *http.Request) {
-		amps := r.PathValue("amps")
-		runCmd(w, "charging-set-amps", amps)
+	var sem = semaphore.NewWeighted(int64(1))
+
+	mux.HandleFunc("/charging-set-amps", func(w http.ResponseWriter, r *http.Request) {
+		sem.Acquire(r.Context(), 1)
+		defer sem.Release(1)
+	
+		amps, _ := io.ReadAll(r.Body)
+		intAmps, err := strconv.Atoi(string(amps))
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprint(w, "Error: ", err)
+			return
+		}
+
+		fmt.Print("Setting amps to: ", intAmps, " ...")
+		handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+			return v.SetChargingAmps(ctx, int32(intAmps))
+		}, true)
 	})
 
 	mux.HandleFunc("/charging-start", func(w http.ResponseWriter, r *http.Request) {
-		runCmd(w, "charging-start")
+		sem.Acquire(r.Context(), 1)
+		defer sem.Release(1)
+		fmt.Print("Starting charge ... ")
+		handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+			return v.ChargeStart(ctx)
+		}, true)
 	})
 
 	mux.HandleFunc("/charging-stop", func(w http.ResponseWriter, r *http.Request) {
-		runCmd(w, "charging-stop")
+		sem.Acquire(r.Context(), 1)
+		defer sem.Release(1)
+		fmt.Print("Stopping charge ...")
+		handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+			return v.ChargeStop(ctx)
+		}, true)
 	})
 
-	mux.HandleFunc("/charging-start-stop/{value}", func(w http.ResponseWriter, r *http.Request) {
-		value := r.PathValue("value")
+	mux.HandleFunc("/charging-start-stop", func(w http.ResponseWriter, r *http.Request) {
+		sem.Acquire(r.Context(), 1)
+		defer sem.Release(1)
+		v, _ := io.ReadAll(r.Body)
+		value := string(v)
 		if value == "start" || value == "true" || value == "on" || value == "1" {
-			runCmd(w, "charging-start")
-		} else {
-			runCmd(w, "charging-stop")
+			fmt.Print("Starting charge ... ")
+			handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+				return v.ChargeStart(ctx)
+			}, true)
+		}else {
+			fmt.Print("Stopping charge ... ")
+			handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+				return v.ChargeStop(ctx)
+			}, true)
 		}
 	})
 
-	//Charging port open/close
-	mux.HandleFunc("/charge-port-open", func(w http.ResponseWriter, r *http.Request) {
-		runCmd(w, "charge-port-open")
-	})
+	mux.HandleFunc("/wake", func(w http.ResponseWriter, r *http.Request) {
+		sem.Acquire(r.Context(), 1)
+		defer sem.Release(1)
 
-	mux.HandleFunc("/charge-port-close", func(w http.ResponseWriter, r *http.Request) {
-		runCmd(w, "charge-port-close")
-	})
-
-	//Charging port open/close
-	mux.HandleFunc("/charge-port-open-close/{value}", func(w http.ResponseWriter, r *http.Request) {
-		value := r.PathValue("value")
-		if value == "open" || value == "true" || value == "on" || value == "1" {
-			runCmd(w, "charge-port-open")
-		} else {
-			runCmd(w, "charge-port-close")
-		}
+		fmt.Print("Waking up car ...")
+		handleFunc(w, r, func(v *vehicle.Vehicle, ctx context.Context) error {
+			return v.Wakeup(ctx)
+		},false)
 	})
 
 	http.ListenAndServe(":8090", mux)
